8fdd8ad35d36e87521e4abcd6c87f19870eb6c6b
wasim-faris <waseemfaris179@gmail.com>
2026-06-08 10:17:16 +0530

    Add linked list insertion at beginning and traversal

--- linked_list.py
+++ linked_list.py
@@ -17,31 +17,72 @@
 #     print(temp.data , end=" ")
 #     temp = temp.next
 
-class Node:
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+
+# a = Node(10)
+# b = Node(20)
+# c = Node(30)
+
+# a.next = b
+# b.next = c
+
+# # Insert 5 at beginning
+# new_node = Node(5)
+# new_node.next = a
+# a = new_node
+
+# # Print list
+# temp = a
+
+# while temp:
+#     print(temp.data, end=" -> ")
+#     temp = temp.next
+
+# print("None")
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+        
+# a = Node(10)
+# b = Node(20)
+# c = Node(30)
+
+# a.next = a
+# b.next = c
+
+# new_node = Node(5)
+# new_node.next = a
+# a = new_node
+
+class Node():
     def __init__(self, data):
         self.data = data
         self.next = None
-
-
+        
 a = Node(10)
-b = Node(20)
-c = Node(30)
+b = Node(15)
+c = Node(20)
+d = Node(30)
 
 a.next = b
 b.next = c
+c.next = d
 
-# Insert 5 at beginning
 new_node = Node(5)
 new_node.next = a
 a = new_node
 
-# Print list
 temp = a
-print(a.data)
 
 while temp:
     print(temp.data, end=" -> ")
     temp = temp.next
-
-print("None")
     
+print("None")
