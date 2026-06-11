de0baa38f7b1fa07846c969d010f8bfa1e588b1f
wasim-faris <waseemfaris179@gmail.com>
2026-06-11 16:48:17 +0530

    Implement linear search first and last occurrence

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,14 @@
-arr = [10, 15, 20, 25, 30]
-target = 25
+arr = [10, 20, 30, 20, 40]
+target = 10
+
+last_occ = -1
+target = 20
 
 for i in range(len(arr)):
     if arr[i]==target:
-        print("Element Found")
-        break
+        last_occ = i
+
+if last_occ !=-1:
+    print(f"Last occurnce at {last_occ}")
+else:
+    print("Not found")
