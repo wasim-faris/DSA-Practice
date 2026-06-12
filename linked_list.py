c9ad9a6260e46bb3d12bbcae782e4a383fafae01
wasim-faris <waseemfaris179@gmail.com>
2026-06-12 14:26:56 +0530

    Remove duplicate nodes from linked list using hash set

--- linked_list.py
+++ linked_list.py
@@ -452,6 +452,43 @@
 # print(largest.data)
 
 
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+        
+# a = Node(10)
+# b = Node(20)
+# c = Node(40)
+# d = Node(50)
+# e = Node(60)
+
+# a.next = b
+# b.next = c
+# c.next = d
+# d.next = e
+
+
+# head = a 
+# temp = head
+
+# new_node = Node(30)
+
+# while temp:
+#     if temp.data == 20:
+#         new_node.next = temp.next
+#         temp.next = new_node
+#     temp = temp.next
+    
+# temp = head 
+
+
+# while temp:
+#     print(temp.data, end=" -> ")
+#     temp = temp.next
+    
+# print("None")
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -459,32 +496,37 @@ class Node:
         
 a = Node(10)
 b = Node(20)
-c = Node(40)
-d = Node(50)
-e = Node(60)
+c = Node(30)
+d = Node(40)
+e = Node(50)
+f = Node(20)
 
 a.next = b
 b.next = c
 c.next = d
 d.next = e
+e.next = f
 
-
-head = a 
+head = a
 temp = head
 
-new_node = Node(30)
+seen = set()
+
+prev = None
 
 while temp:
-    if temp.data == 20:
-        new_node.next = temp.next
-        temp.next = new_node
+    if temp.data in seen:
+        prev.next = temp.next
+    else:
+        seen.add(temp.data)
+        prev = temp
+        
     temp = temp.next
     
-temp = head 
-
+temp = head
 
 while temp:
-    print(temp.data, end=" -> ")
+    print(temp.data, end="-> ")
     temp = temp.next
     
-print("None")
+print('None')
