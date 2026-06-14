b3e43a66378f370ff5b3addf8a15fd389c045c76
wasim-faris <waseemfaris179@gmail.com>
2026-06-14 17:08:36 +0530

    Add backward traversal for doubly linked list

--- linked_list.py
+++ linked_list.py
@@ -590,39 +590,64 @@
 #         right = mid - 1
         
         
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+        
+# arr = [1,2,36,7,3,4,65,67, 2]
+
+# head = Node(arr[0])
+# temp = head
+# for i in range(1, len(arr)):
+#     next_node = Node(arr[i])
+#     temp.next = next_node
+#     temp = next_node
+    
+# temp = head
+
+# prev = None
+# seen = set()
+
+# while temp:
+#     if temp.data in seen:
+#         prev.next = temp.next
+#     else:
+#         seen.add(temp.data)
+#         prev = temp
+#     temp = temp.next
+    
+# temp = head 
+
+# while temp:
+#     print(temp.data, end="->")
+#     temp = temp.next
+# print("None")
+
 class Node:
-    def __init__(self, data):
+    def __init__(self,data):
         self.data = data
         self.next = None
+        self.prev = None
         
-arr = [1,2,36,7,3,4,65,67, 2]
+a = Node(10)
+b = Node(20)
+c = Node(30)
+d = Node(40)
 
-head = Node(arr[0])
-temp = head
-for i in range(1, len(arr)):
-    next_node = Node(arr[i])
-    temp.next = next_node
-    temp = next_node
-    
-temp = head
+a.next = b
+b.next = c
+c.next = d
 
-prev = None
-seen = set()
+b.prev = a
+c.prev = b
+d.prev = c
 
-while temp:
-    if temp.data in seen:
-        prev.next = temp.next
-    else:
-        seen.add(temp.data)
-        prev = temp
-    temp = temp.next
-    
-temp = head 
+temp = d
 
 while temp:
-    print(temp.data, end="->")
-    temp = temp.next
-print("None")
-
-
+    print(temp.data, end="<-")
+    temp = temp.prev
+    
+print("Nonne")
 
