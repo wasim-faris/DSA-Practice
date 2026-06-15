adad0a7656e0282f418a56c1b349dc14c9f1de28
wasim-faris <waseemfaris179@gmail.com>
2026-06-15 21:03:53 +0530

    Add solution for removing duplicates from linked list

--- linked_list.py
+++ linked_list.py
@@ -624,30 +624,134 @@
 #     temp = temp.next
 # print("None")
 
+# class Node:
+#     def __init__(self,data):
+#         self.data = data
+#         self.next = None
+#         self.prev = None
+        
+# a = Node(10)
+# b = Node(20)
+# c = Node(30)
+# d = Node(40)
+
+# a.next = b
+# b.next = c
+# c.next = d
+
+# b.prev = a
+# c.prev = b
+# d.prev = c
+
+# temp = d
+
+# while temp:
+#     print(temp.data, end="<-")
+#     temp = temp.prev
+    
+# print("Nonne")
+
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
         self.prev = None
         
-a = Node(10)
-b = Node(20)
-c = Node(30)
-d = Node(40)
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+b.prev = a
+c.prev = b
+d.prev = c
+e.prev = d
+
+value = 3
+new_value = Node(2333)
+head = a 
+temp = head
+
+# while temp:
+#     if temp.data == value:
+#         new_value.next = temp.next
+#         temp.next = new_value
+        
+#     temp = temp.next
+    
+# temp = head
+
+# while temp:
+#     print(temp.data, end="->")
+#     temp = temp.next
+# print("None")
+
+# class Node:
+#     def __init__(self,data):
+#         self.data = data
+#         self.next = None
+        
+# a = Node(1)
+# b = Node(2)
+# c = Node(3)
+# d = Node(4)
+
+# a.next = b
+# b.next = c
+# c.next = d
+
+# delete_value = 3
+# head = a 
+# temp = head
+
+# while temp.next:
+#     if temp.next.data == delete_value:
+#         temp.next = temp.next.next
+#     temp = temp.next
+    
+
+# temp = head
+
+# while temp:
+#     print(temp.data, end="->")
+#     temp = temp.next
+# print("None")
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+a = Node(12)
+b = Node(13)
+c = Node(14)
+d = Node(15)
 
 a.next = b
 b.next = c
 c.next = d
 
-b.prev = a
-c.prev = b
-d.prev = c
+head = a 
 
-temp = d
+temp = head 
+
+seen = set()
 
 while temp:
-    print(temp.data, end="<-")
-    temp = temp.prev
-    
-print("Nonne")
+    if temp.data in seen:
+        prev.next = temp.next
+    else:
+        seen.add(temp.data)
+        prev = temp
+        
+temp = head
 
+while temp:
+    print(temp.data, end="-> ")
+    temp = temp.next
