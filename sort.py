33845798463f778cb1b37dc4670ec97a6fdfd425
wasim-faris <waseemfaris179@gmail.com>
2026-07-02 09:13:46 +0530

    Implement insertion sort algorithm

--- sort.py
+++ sort.py
@@ -1,21 +1,17 @@
 
 
 
-arr = [5, 2, 4, 6, 1, 3]
 
+arr = [2,5,2,7,6,9,8,3]
 n = len(arr)
-
-for i in range(1, n):
-
+for i in range(1,n):
     key = arr[i]
-    j = i - 1
+    j = i-1
+    while j>=0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j -=1
 
-    while j >= 0 and arr[j] > key:
-
-        arr[j + 1] = arr[j]
-        j -= 1
-
-    arr[j + 1] = key
+    arr[j+1] = key
 
 
 print(arr)
