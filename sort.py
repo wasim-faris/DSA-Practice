605abc73e4f0413b3bea95bb9e6ccf7198ac4411
wasim-faris <waseemfaris179@gmail.com>
2026-07-09 23:03:18 +0530

    dsa: implement insertion sort algorithm

--- sort.py
+++ sort.py
@@ -17,22 +17,16 @@
 # print(arr)
 
 
-arr = [2,3,4,5,1,6,7,8,]
-
-
+arr = [1,2,3,4,5,6,89]
 n = len(arr)
-
-
 for i in range(1, n):
     key = arr[i]
     j = i-1
-    
-    while j >= 0 and arr[j] > key:
+    while j>= 0 and arr[j] > key:
         arr[j+1] = arr[j]
-        j-=1
+        j -=1
 
         arr[j+1] = key
 
 print(arr)
-
 
